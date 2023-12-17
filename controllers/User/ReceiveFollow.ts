d66665905eddf5cf81bb9749010bc69449f01718
Mohamed Brzan <mohamedbrzan11@gmail.com>
2023-12-17 07:24:04 +0200

    fix user routes because req.user bug

--- controllers/User/ReceiveFollow.ts
+++ controllers/User/ReceiveFollow.ts
@@ -5,45 +5,45 @@ import ErrorHandler from '../../middleware/ErrorHandler';
 
 export default AsyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    // const { follower } = req.body;
+    const { follower } = req.body;
 
-    // let user = await User.findById(req['user']._id).select(
-    //   'followers name _id'
-    // );
+    let user = await User.findById(req.user['id']).select(
+      'followers name _id'
+    );
 
-    // if (follower === user._id.toString())
-    //   return next(new ErrorHandler(500, 'You Cannot Follow Yourself'));
+    if (follower === user._id.toString())
+      return next(new ErrorHandler(500, 'You Cannot Follow Yourself'));
 
-    // let following = await User.findById(follower).select('followings name');
+    let following = await User.findById(follower).select('followings name');
 
-    // const userIndex = following?.followings?.findIndex(
-    //   (f) => f.toString() === req['user']._id
-    // );
+    const userIndex = following?.followings?.findIndex(
+      (f) => f.toString() === req.user['id']
+    );
 
-    // if (userIndex)
-    //   return next(
-    //     new ErrorHandler(500, `${user?.name} Is Already In Your Followings`)
-    //   );
+    if (userIndex)
+      return next(
+        new ErrorHandler(500, `${user?.name} Is Already In Your Followings`)
+      );
 
-    // let followingIndex = user?.followers?.findIndex(
-    //   (f) => f.toString() === follower
-    // );
+    let followingIndex = user?.followers?.findIndex(
+      (f) => f.toString() === follower
+    );
 
-    // if (followingIndex)
-    //   return next(
-    //     new ErrorHandler(500, `${following?.name} Is Already In Your Followers`)
-    //   );
+    if (followingIndex)
+      return next(
+        new ErrorHandler(500, `${following?.name} Is Already In Your Followers`)
+      );
 
-    // //* Following The User
-    // following?.followings?.push(req['user']._id);
-    // await following.save();
+    //* Following The User
+    following?.followings?.push(req.user['id']);
+    await following.save();
 
-    // //* Follow The User
-    // user?.followers?.push(follower);
-    // await user.save();
+    //* Follow The User
+    user?.followers?.push(follower);
+    await user.save();
 
-    // return res.status(200).json({
-    //   message: `${following?.name} Follow ${user?.name} Successfully`,
-    // });
+    return res.status(200).json({
+      message: `${following?.name} Follow ${user?.name} Successfully`,
+    });
   }
 );
--- controllers/User/SendFollow.ts
+++ controllers/User/SendFollow.ts
@@ -5,45 +5,45 @@ import ErrorHandler from '../../middleware/ErrorHandler';
 
 export default AsyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    // const { following } = req.body;
+    const { following } = req.body;
 
-    // let user = await User.findById(req['user']._id).select(
-    //   'followings name _id'
-    // );
+    let user = await User.findById(req.user['id']).select(
+      'followings name _id'
+    );
 
-    // if (following === user._id.toString())
-    //   return next(new ErrorHandler(500, 'You Cannot Follow Yourself'));
+    if (following === user._id.toString())
+      return next(new ErrorHandler(500, 'You Cannot Follow Yourself'));
 
-    // let follower = await User.findById(following).select('followers name');
+    let follower = await User.findById(following).select('followers name');
 
-    // const userIndex = follower?.followers?.findIndex(
-    //   (f) => f.toString() === req['user']._id
-    // );
+    const userIndex = follower?.followers?.findIndex(
+      (f) => f.toString() === req.user['id']
+    );
 
-    // if (follower?.followers?.length > 0 && userIndex)
-    //   return next(
-    //     new ErrorHandler(500, `${follower.name} Is Already In Your Followings`)
-    //   );
+    if (follower?.followers?.length > 0 && userIndex)
+      return next(
+        new ErrorHandler(500, `${follower.name} Is Already In Your Followings`)
+      );
 
-    // let followingIndex = user?.followings?.findIndex(
-    //   (f) => f.toString() === following
-    // );
+    let followingIndex = user?.followings?.findIndex(
+      (f) => f.toString() === following
+    );
 
-    // if (user?.followings?.length > 0 && followingIndex)
-    //   return next(
-    //     new ErrorHandler(500, `${following?.name} Is Already In Your Followers`)
-    //   );
+    if (user?.followings?.length > 0 && followingIndex)
+      return next(
+        new ErrorHandler(500, `${following?.name} Is Already In Your Followers`)
+      );
 
-    // //* Following The User
-    // follower.followers.push(req['user']._id);
-    // await follower.save();
+    //* Following The User
+    follower.followers.push(req.user['id']);
+    await follower.save();
 
-    // //* Follow The User
-    // user.followings.push(following);
-    // await user.save();
+    //* Follow The User
+    user.followings.push(following);
+    await user.save();
 
-    // return res.status(200).json({
-    //   message: `You (${user?.name}) Follow ${follower?.name} Successfully`,
-    // });
+    return res.status(200).json({
+      message: `You (${user?.name}) Follow ${follower?.name} Successfully`,
+    });
   }
 );
