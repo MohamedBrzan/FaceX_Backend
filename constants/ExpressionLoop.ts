1c603ed11bced5d04669abf64e1c644ed0eb0ea8
Mohamed Brzan <mohamedbrzan11@gmail.com>
2023-12-21 23:20:53 +0200

    update ExpressionLoop function to return a boolean value

--- constants/ExpressionLoop.ts
+++ constants/ExpressionLoop.ts
@@ -1,4 +1,4 @@
-export default async (userId: string, modelName: any): Promise<void> => {
+export default async (userId: string, modelName: any): Promise<boolean> => {
   try {
     let keys = [
       'like',
@@ -12,16 +12,21 @@ export default async (userId: string, modelName: any): Promise<void> => {
       'fear',
     ];
 
+    let founded: boolean = false;
+
     for (const name of keys) {
       do {
         for (let i = 0; i < modelName.expressions[name].length; i++) {
           if (userId === modelName.expressions[name][i].toString()) {
             modelName.expressions[name].splice(i, 1);
             await modelName.save();
+            founded = true;
           }
         }
       } while (false);
     }
+
+    return founded;
   } catch (error) {
     throw new Error(error);
   }
