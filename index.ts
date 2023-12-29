ef8514e0446c409b4c2b279c5e2721b249df4037
MohamedMahmoud <83614946+MohamedBrzan@users.noreply.github.com>
2023-12-29 13:34:58 +0200

    fix index.ts conflict

--- index.ts
+++ index.ts
@@ -146,14 +146,13 @@ app.use('/comment', Comment);
 app.use('/reply', Reply);
 app.use('/reel', Reel);
 app.use('/blog', Blog);
-app.use('/video', Video);
 app.use('/job', Job);
-// app.use('/hashTag', HashTag);
-app.use('/image', Image);
-app.use('/album', Album);
+app.use('/hashTag', HashTag);
 // app.use('/ad', Ad);
-
+// app.use('/image', Image);
+// app.use('/album', Album);
 // app.use('/notification', Notification);
+// app.use('/video', Video);
 // app.use('/payment', Payment);
 
 app.use(ErrorMessage);
