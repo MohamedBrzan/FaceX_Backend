4f0b613e8e277650984a04e0ad08369625b1c784
MohamedMahmoud <83614946+MohamedBrzan@users.noreply.github.com>
2023-12-29 14:07:13 +0200

    update CreateHashtag function

--- controllers/HashTag/CreateHashTag.ts
+++ controllers/HashTag/CreateHashTag.ts
@@ -3,23 +3,33 @@ import AsyncHandler from '../../middleware/AsyncHandler';
 import HashTag from '../../models/HashTag/HashTag';
 import User from '../../models/User/User';
 import ErrorHandler from '../../middleware/ErrorHandler';
+import { getUserId } from '../../constants/UserId';
 
 export default AsyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     const { text } = req.body;
 
-    let user = await User.findById(req['authorizedUser']._id);
+    const userId = (await getUserId(req)).toString();
+
+    let user = await User.findById(userId);
 
     if (!user)
       return next(new ErrorHandler(404, `You Must Be Logged In First`));
 
-    let hashTag = await HashTag.create({ user: req['authorizedUser']._id, text });
-
-    await User.findByIdAndUpdate(req['authorizedUser']._id, {
-      $push: {
-        'hashTags.create': hashTag['_id'].toString(),
-      },
+    let hashTag = await HashTag.create({
+      user: userId,
+      text,
     });
+
+    await User.findByIdAndUpdate(
+      userId,
+      {
+        $push: {
+          'hashTags.published': hashTag.toString(),
+        },
+      },
+      { new: true, runValidators: true, upsert: true }
+    );
 
     return res.status(200).json(hashTag);
   }
