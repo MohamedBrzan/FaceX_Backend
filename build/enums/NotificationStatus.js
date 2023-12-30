657fa52dc265d9143c27903083979815ccb732d6
MohamedMahmoud <83614946+MohamedBrzan@users.noreply.github.com>
2023-12-30 07:57:35 +0200

    Delete build directory

--- build/enums/NotificationStatus.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var NotificationStatus;
-(function (NotificationStatus) {
-    NotificationStatus["Read"] = "read";
-    NotificationStatus["Unread"] = "unread";
-})(NotificationStatus || (NotificationStatus = {}));
-exports.default = NotificationStatus;
--- build/review.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logout = void 0;
-let logout = (req, res) => {
-    req.logout();
-    res.redirect('/');
-};
-exports.logout = logout;
