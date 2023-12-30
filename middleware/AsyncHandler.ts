500ea84f3a7e57fcec10ae85ad043c19a77e0bdb
MohamedMahmoud <83614946+MohamedBrzan@users.noreply.github.com>
2023-12-30 07:59:50 +0200

    Delete middleware directory

--- middleware/AsyncHandler.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export default (controllers: Function) =>
-  (req: Request, res: Response, next: NextFunction) =>
-    Promise.resolve(controllers(req, res, next)).catch(next);
