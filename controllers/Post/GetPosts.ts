1ef1de32cac2e320d5d934d0f9417d6119eccd1d
MohamedBrzan <mohamedbrzan11@gmail.com>
2024-01-15 00:02:02 +0200

    make show up just the user name and avatar moreover _id in the post object

--- controllers/Post/GetPosts.ts
+++ controllers/Post/GetPosts.ts
@@ -4,5 +4,50 @@ import Post from '../../models/Post/Post';
 
 export default AsyncHandler(
   async (req: Request, res: Response, next: NextFunction) =>
-    res.status(200).json(await Post.find().populate('user'))
+    res.status(200).json(
+      await Post.find().populate([
+        { path: 'user', select: 'name avatar' },
+        {
+          path: 'expressions',
+          populate: [
+            {
+              path: 'angry',
+              select: 'name avatar',
+            },
+            {
+              path: 'disgust',
+              select: 'name avatar',
+            },
+            {
+              path: 'fear',
+              select: 'name avatar',
+            },
+            {
+              path: 'happy',
+              select: 'name avatar',
+            },
+            {
+              path: 'like',
+              select: 'name avatar',
+            },
+            {
+              path: 'love',
+              select: 'name avatar',
+            },
+            {
+              path: 'sad',
+              select: 'name avatar',
+            },
+            {
+              path: 'support',
+              select: 'name avatar',
+            },
+            {
+              path: 'surprise',
+              select: 'name avatar',
+            },
+          ],
+        },
+      ])
+    )
 );
